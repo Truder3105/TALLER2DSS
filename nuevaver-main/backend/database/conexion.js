41bc835f6c6a42ec82ac601481f72010fa505dbc
Truder3105 <83443150+Truder3105@users.noreply.github.com>
2024-10-02 18:29:26 -0500

    Actualización 2/10

--- nuevaver-main/backend/database/conexion.js
+++ nuevaver-main/backend/database/conexion.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql2");
-const db = mysql.createConnection ({
-    host: "127.0.0.1",
-    user: "jpgarzon",
-    password: "juan1973",
-    database: "cursos",
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "0000",
+  database: "cursos",
 });
 
-db.connect((err)=> {
-    if (err) {
-        throw err;
-    }
-    console.log ("BD Mysql Conectado");
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("BD Mysql Conectado");
 });
 
 module.exports = db;
--- nuevaver-main/frontend/scripstP.js
+++ nuevaver-main/frontend/scripstP.js
@@ -13,35 +13,6 @@ function guardar() {
     nombre: document.getElementById("nombre").value,
     apellidos: document.getElementById("apellidos").value,
     email: document.getElementById("correo").value,
-  });
-
-  let requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
-
-  fetch("http://localhost:8888/.netlify/functions/estudiantes", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
-}
-function guardarP() {
-  let nota = 0.0;
-  let apellidos = "";
-
-  let datoingresado = document.getElementById("correo").value;
-
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  event.preventDefault();
-
-  let raw = JSON.stringify({
-    dni: document.getElementById("dni").value,
-    nombre: document.getElementById("nombre").value,
-    apellidos: document.getElementById("apellidos").value,
-    email: document.getElementById("correo").value,
     profesor: document.getElementById("profesor").value,
     telefono: document.getElementById("telefono").value,
   });
@@ -70,6 +41,8 @@ function cargar(resultado) {
     elemento = elemento + "<br>Nombres: " + transformado[vc].nombre;
     elemento = elemento + "<br>Apellidos: " + transformado[vc].apellidos;
     elemento = elemento + "<br>Correo electrónico: " + transformado[vc].email;
+    elemento = elemento + "<br>Profesor: " + transformado[vc].profesor;
+    elemento = elemento + "<br>Telefono: " + transformado[vc].telefono;
     salida = salida + elemento + "<br><br>";
   }
   document.getElementById("rta").innerHTML = salida;
@@ -81,7 +54,7 @@ function listar() {
     method: "GET",
     redirect: "follow",
   };
-  fetch("http://localhost:8888/.netlify/functions/estudiantes", requestOptions)
+  fetch("http://localhost:8888/.netlify/functions/profesores", requestOptions)
     .then((response) => response.text())
     .then((result) => cargar(result))
     .catch((error) => console.error(error));
@@ -101,36 +74,8 @@ function actualizar() {
     nombre: document.getElementById("nombreA").value,
     apellidos: document.getElementById("apellidosA").value,
     email: document.getElementById("correoA").value,
-  });
-
-  let requestOptions = {
-    method: "PUT",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
-  let elid = document.getElementById("idA").value;
-  fetch(
-    "http://localhost:8888/.netlify/functions/estudiantes/" + elid,
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then((result) => respuesta_actualizar(result))
-    .catch((error) => console.error(error));
-}
-
-function actualizarP() {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  event.preventDefault();
-
-  let raw = JSON.stringify({
-    dni: document.getElementById("dniA").value,
-    nombre: document.getElementById("nombreA").value,
-    apellidos: document.getElementById("apellidosA").value,
-    email: document.getElementById("correoA").value,
     profesor: document.getElementById("profesorA").value,
-    email: document.getElementById("telfonoA").value,
+    telefono: document.getElementById("telefonoA").value,
   });
 
   let requestOptions = {
@@ -158,31 +103,13 @@ function cargarLE(resultado) {
   elemento = elemento + "<br>Nombres: " + transformado.nombre;
   elemento = elemento + "<br>Apellidos: " + transformado.apellidos;
   elemento = elemento + "<br>Correo electrónico: " + transformado.email;
+  elemento = elemento + "<br>Profesor: " + transformado.profesor;
+  elemento = elemento + "<br>Telefono: " + transformado.telefono;
   salida = salida + elemento + "<br><br>";
   document.getElementById("rtaLE").innerHTML = salida;
 }
 
 function listar_estudiante() {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  event.preventDefault();
-
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-  let elid = document.getElementById("idLE").value;
-  fetch(
-    "http://localhost:8888/.netlify/functions/estudiantes/" + elid,
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then((result) => cargarLE(result))
-    .catch((error) => console.error(error));
-}
-
-function listar_profesores() {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   event.preventDefault();
@@ -219,7 +146,7 @@ function eliminar_estudiante() {
   };
   let elid = document.getElementById("idEE").value;
   fetch(
-    "http://localhost:8888/.netlify/functions/estudiantes/" + elid,
+    "http://localhost:8888/.netlify/functions/profesores/" + elid,
     requestOptions
   )
     .then((response) => response.text())
